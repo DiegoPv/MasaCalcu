9dce053119c2163800c9d7dfe84870400e0ee4fa
ale callejo <ale callejo>
2016-04-09 17:28:12 -0300

    subo primer release

---
+++ TP1-Calculadora/src/Calculadora.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+public class Calculadora 
+{	
+	public Calculadora () {}					
+						
+	public void Calcular()
+	{
+		String cadena;
+		Scanner sc = new Scanner(System.in);		
+		cadena= sc.nextLine();		
+		sc.close();
+		StringTokenizer st= new StringTokenizer (cadena, "+-", true);		
+		
+		int vnum= 0;
+		int vop= 0;
+		Vector <Integer> operandos = new Vector <Integer>(); //numero
+		Vector <String> operadores = new Vector <String>();	 //operacion
+		
+		boolean valor= true;
+		
+		while (st.hasMoreTokens ())
+		{			
+			if(valor)
+			{   
+				operandos.add(Integer.parseInt(st.nextToken()));               	
+				valor=false;
+				vnum++;
+			}
+			else                                                        
+			{				
+				operadores.add(st.nextToken());                                
+				valor=true;
+				vop++;
+			}
+		}		
+		int contador=0;
+		double resultado= 0.0;
+		resultado=operandos.elementAt(0);
+		
+		do 
+		{
+			if (operadores.elementAt(contador).compareTo("+")==0)				
+			{				
+				resultado= resultado + operandos.elementAt(contador + 1);				
+			}
+			else
+			{
+				resultado= resultado - operandos.elementAt(contador + 1);
+			}
+			contador++;
+			vop--;		
+		}
+		while (vop != 0);
+		System.out.println ("El resultado es: "+resultado);			
+	}	
+}
---
+++ TP1-Calculadora/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{		
+		Calculadora masacre= new Calculadora ();
+		
+		System.out.println("Bienvenido a la calculadora!! v1.0.0");
+		System.out.println ("Ingrese la operación a realizar: ");
+		masacre.Calcular();			
+	}
+}
