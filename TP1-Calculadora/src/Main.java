2e0b04c84aea0ae6973467549f9a55632a11dc1d
Manuel Esperanza <sojaku@gmail.com>
2016-04-10 18:51:37 -0300

    subimos el release v2.1.0 a la branch dev_v2.x.x

--- TP1-Calculadora/src/Main.java
+++ TP1-Calculadora/src/Main.java
@@ -1,11 +1,11 @@
 
 public class Main 
 {
-	public static void main(String[] args) 
+	public static void main (String[] args) 
 	{		
 		Calculadora masacre= new Calculadora ();
 		
-		System.out.println("Bienvenido a la calculadora!! v2.0.0");
+		System.out.println("Bienvenido a la calculadora!! v2.1.0");
 		System.out.println ("Ingrese la operación a realizar: ");
 		masacre.Calcular();			
 	}
